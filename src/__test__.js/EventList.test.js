6a2935213c39ccbaa2b314a2631628ecd00e859c
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-02-19 15:25:04 -0600

    updated eventlist.test.js

--- src/__test__.js/EventList.test.js
+++ src/__test__.js/EventList.test.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
+import { getEvents } from '../api'
 
 describe('<EventList /> component', () => {
     test('has an element with "list" role', () => {
@@ -7,9 +8,10 @@ describe('<EventList /> component', () => {
         expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
     });
 
-    test('renders correct number of events', () => {
-        const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+    test('renders correct number of events', async () => {
+        const allEvents = await getEvents();
+        const EventListComponent = render(<EventList />);
+        EventListComponent.rerender(<EventList events={allEvents} />);
+        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
     });
 });
-
